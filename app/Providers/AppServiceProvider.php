81be285513cea2a6f8ffc349b7672d6f471ab241
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-07-09 21:43:17 +0700

    laravel installation

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Events\QueryExecuted;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        DB::listen(function(QueryExecuted $query){
+            Log::info($query->sql);
+        });
+    }
+}
